bbdb19f33f8c35253f7177d41184ec4f33978d95
Adrien Lemaire <lemaire.adrien@gmail.com>
2011-01-31 09:06:27 +0000

    logging + conditions with datetime

--- bin/sleep.py
+++ bin/sleep.py
@@ -9,17 +9,31 @@ if os.path.exists(activate_this):
     execfile(activate_this, dict(__file__=activate_this))
 
 import datetime
+import logging
 import subprocess
 
+# constants
+LOG_DIR = '/Users/Fandekasp/Documents/Programming/Python/LucidDreamingBox/'
 
-def plan_waking():
+# logging
+log_file = os.path.join(LOG_DIR, "dream.log")
+logging.basicConfig(filename=log_file, level=logging.DEBUG)
+logger = logging.getLogger('sleep')
+
+
+def plan_waking(sleep_time, interval):
     """configure a system wake in 5 hours"""
-    sleep_time = datetime.datetime.now()
-    wake_time = sleep_time + datetime.timedelta(hours=5)
+    wake_time = sleep_time + datetime.timedelta(hours=interval)
     #wake_time = sleep_time + datetime.timedelta(seconds=15)
     wake_str = wake_time.strftime("%m/%d/%y %H:%M:%S")
+    logger.info("System will wake up on %s" % wake_str)
     cmd = 'pmset schedule wake'.split() + [wake_str]
-    subprocess.call(cmd)
+    logger.debug(subprocess.call(cmd))
 
 
-plan_waking()
+sleep_time = datetime.datetime.now()
+logger.info("script launched, sleep_time.hour = %d" % sleep_time.hour)
+if 23 <= sleep_time.hour or sleep_time.hour <= 2:
+    plan_waking(sleep_time, 5)
+elif 5 <= sleep_time.hour <= 10:
+    plan_waking(sleep_time, 1)
--- bin/wake.py
+++ bin/wake.py
@@ -9,8 +9,19 @@ if os.path.exists(activate_this):
     execfile(activate_this, dict(__file__=activate_this))
 
 from appscript import app
+import datetime
+import logging
 import random
+import subprocess
 from threading import Timer
+
+# constants
+LOG_DIR = '/Users/Fandekasp/Documents/Programming/Python/LucidDreamingBox/'
+
+# logging
+log_file = os.path.join(LOG_DIR, "dream.log")
+logging.basicConfig(filename=log_file, level=logging.DEBUG)
+logger = logging.getLogger('wake')
 
 
 def launch_itunes():
@@ -19,7 +30,23 @@ def launch_itunes():
     list_tracks = iTunes.playlists["Best rates"].tracks.get()
     track_to_play = random.choice(list_tracks)
     INTERVAL = 5
+    logger.info("iTunes called")
+    logger.debug("%s will be played in %d seconds" %
+            (track_to_play.name.get(), INTERVAL))
     Timer(INTERVAL, track_to_play.play, ()).start()
-    print "OK2"
 
-launch_itunes()
+
+def launch_dream_latex():
+    """Open vim, fill it with a latex template, then gives focus"""
+    cmd = "/usr/local/bin/mvim /Users/Fandekasp/Documents/testdream.tex --cmd"
+    logger.info("Vim called")
+    logger.debug(cmd.split() + [':se lines=40 columns=80'])
+    subprocess.call(cmd.split() + [':se lines=40 columns=80'])
+
+
+now = datetime.datetime.now()
+logger.info("wake script called at %s" % now.strftime("%H:%M:%S"))
+if 5 <= now.hour <= 10:
+    """If it's the morning"""
+    launch_itunes()
+    launch_dream_latex()
